8ed615f9753c5f9195a8dd53bee79079a7cb87a1
Nahum Alon <nochum.shepsl@gmail.com>
2024-07-25 17:37:21 +0300

    Project created

---
+++ src/main/java/telran/strings/Strings.java
@@ -0,0 +1,13 @@
+package telran.strings;
+
+public class Strings {
+
+    public static String firstName() {
+        return "[A-Z][a-z]{4,}";
+    }
+
+    public static String javaVariable() {
+        //TODO
+        return "";
+    }
+}
---
+++ src/test/java/telran/strings/test/StringsTest.java
@@ -0,0 +1,13 @@
+package telran.strings.test;
+
+import static telran.strings.Strings.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringsTest {
+
+    @Test
+    void javaVariableTest() {
+        // TODO
+    }
+}
