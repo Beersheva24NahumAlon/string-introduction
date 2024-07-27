654e83c08aa8eb69625b3d103b037629a77c0b30
Nahum Alon <nochum.shepsl@gmail.com>
2024-07-27 18:52:01 +0300

    Fixed regular expression in method javaVariable (it could be easer)

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -7,6 +7,6 @@ public class Strings {
     }
 
     public static String javaVariable() {
-        return "(([a-z]|[A-Z]|_|\\$)([a-z]|[A-Z]|[0-9]|_|\\$)+|([a-z]|[A-Z]|\\$)([a-z]|[A-Z]|[0-9]|_|\\$)*)";
+        return "(_([a-z]|[A-Z]|[0-9]|_|\\$)+|([a-z]|[A-Z]|\\$)([a-z]|[A-Z]|[0-9]|_|\\$)*)";
     }
 }
