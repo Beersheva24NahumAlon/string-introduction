33e46cb5fd7b5dc7d27417b9411de7af56ab0a66
Nahum Alon <nochum.shepsl@gmail.com>
2024-07-30 17:45:25 +0300

    State of finish of lesson #11

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -1,12 +1,62 @@
 package telran.strings;
 
+import java.util.Arrays;
+
 public class Strings {
+    static final String keywords[] = { "abstract", "assert", "boolean",
+                "break", "byte", "case", "catch", "char", "class", "const",
+                "continue", "default", "do", "double", "else", "enum", "extends", "false",
+                "final", "finally", "float", "for", "goto", "if", "implements",
+                "import", "instanceof", "int", "interface", "long", "native",
+                "new", "null", "package", "private", "protected", "public",
+                "return", "short", "static", "strictfp", "super", "switch",
+                "synchronized", "this", "throw", "throws", "transient", "true",
+                "try", "void", "volatile", "while" };
 
     public static String firstName() {
-        return "[A-Z][a-z]{4,}";      
+        return "([A-Z][a-z]{4,})";      
     }
 
     public static String javaVariable() {
-        return "(_[\\w$]+)|([a-zA-Z$][\\w$]*)";
+        //return "[a-zA-Z$]|[a-zA-Z_$][\\w$]+";
+        return "((?!_$)[a-zA-Z_$][\\w$]*)";
+    }
+
+    public static String number0_300() {
+        return "0|[1-9]\\d?|[1-2]\\d\\d|300";
+    }
+
+    public static String ipV4Octet() {
+        return "([0-1]?\\d{1,2}|2([0-4]\\d|5[0-5]))";
+    }
+
+    public static String ipV4Address() {
+        String octetExpr = ipV4Octet();
+        return String.format("%s(\\.%s){3}", octetExpr, octetExpr);
+    }
+
+    public static String stringWithJavaNames(String names) {
+        String[] tokens = names.split("\\s+");
+        String res = "";
+        for (String name : tokens) {
+            if (isJavaName(name)) {
+                res += name + " ";
+            }
+        }
+        return res.length() > 0 ? res.substring(0, res.length() - 1) : "";
+    }
+
+    private static boolean isJavaName(String str) {
+        return str.matches(javaVariable()) && !isKeyword(str) ? true : false;
+    }
+
+    private static boolean isKeyword(String str) {
+        return Arrays.binarySearch(keywords, str) >= 0 ? true : false;
+    }
+
+    public static boolean isArithmeticExpression(String expr) {
+        //TODO
+        return false;
+
     }
 }
--- src/test/java/telran/strings/test/StringsTest.java
+++ src/test/java/telran/strings/test/StringsTest.java
@@ -1,6 +1,8 @@
 package telran.strings.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static telran.strings.Strings.*;
 
 import org.junit.jupiter.api.Test;
@@ -9,25 +11,106 @@ public class StringsTest {
 
     @Test
     void firstNameTest() {
-        String[] strPass = { "Nahum" };
-        String[] strFail = { "Nah", "NAhum", "NahuM" };
-        for (String str : strPass) {
-            assertEquals(true, str.matches(firstName()));
-        }
-        for (String str : strFail) {
-            assertEquals(false, str.matches(firstName()));
-        }
+        assertTrue("Nahum".matches(firstName()));
+
+        assertFalse("Nah".matches(firstName()));
+        assertFalse("NAhum".matches(firstName()));
+        assertFalse("NahuM".matches(firstName()));
     }
 
     @Test
     void javaVariableTest() {
-        String[] strPass = { "j", "$" ,"javaVariable", "_Java$Variable$", "$JavaVariable_", "a1"};
-        String[] strFail = { "1java_Variable$", "java%Variable$", "_"};
-        for (String str : strPass) {
-            assertEquals(true, str.matches(javaVariable()));
-        }
-        for (String str : strFail) {
-            assertEquals(false, str.matches(javaVariable()));
-        }
+        assertTrue("j".matches(javaVariable()));
+        assertTrue("$".matches(javaVariable()));
+        assertTrue("javaVariable".matches(javaVariable()));
+        assertTrue("_Java$Variable$".matches(javaVariable()));
+        assertTrue("$JavaVariable_".matches(javaVariable()));
+        assertTrue("a1".matches(javaVariable()));
+
+        assertFalse("1java_Variable$".matches(javaVariable()));
+        assertFalse("java%Variable$".matches(javaVariable()));
+        assertFalse("_".matches(javaVariable()));
+    }
+
+    @Test
+    void number0_300TrueTest() {
+        String regexp = number0_300();
+        assertTrue("123".matches(regexp));
+        assertTrue("0".matches(regexp));
+        assertTrue("300".matches(regexp));
+        assertTrue("12".matches(regexp));
+        assertTrue("25".matches(regexp));
+        assertTrue("299".matches(regexp));
+        assertTrue("1".matches(regexp));
+    }
+
+    @Test
+    void number0_300FalseTest() {
+        String regexp = number0_300();
+        assertFalse("00".matches(regexp));
+        assertFalse("301".matches(regexp));
+        assertFalse("01".matches(regexp));
+        assertFalse("00".matches(regexp));
+        assertFalse("030".matches(regexp));
+        assertFalse("1(".matches(regexp));
+        assertFalse("1000".matches(regexp));
+        assertFalse(" 20".matches(regexp));
+        assertFalse("1001".matches(regexp));
+    }
+
+    @Test
+    void ipV4OctetTrueTest() {
+        String regexp = ipV4Octet();
+        assertTrue("0".matches(regexp));
+        assertTrue("00".matches(regexp));
+        assertTrue("000".matches(regexp));
+        assertTrue("10".matches(regexp));
+        assertTrue("100".matches(regexp));
+        assertTrue("255".matches(regexp));
+        assertTrue("199".matches(regexp));
+        assertTrue("249".matches(regexp));
+        assertTrue("192".matches(regexp));  
+    }
+
+    @Test
+    void ipV4OctetFalseTest() {
+        String regexp = ipV4Octet();
+        assertFalse("0000".matches(regexp));
+        assertFalse("t".matches(regexp));
+        assertFalse("-1".matches(regexp));
+        assertFalse("1111".matches(regexp));
+        assertFalse("0001".matches(regexp));
+        assertFalse("256".matches(regexp));
+        assertFalse("300".matches(regexp));
+        assertFalse("*".matches(regexp));
+        assertFalse("1 ".matches(regexp));
+    }
+
+    @Test
+    void ipV4AddressTrueTest() {
+        String regexp = ipV4Address();
+        assertTrue("0.0.0.0".matches(regexp));
+        assertTrue("192.168.0.1".matches(regexp));
+        assertTrue("255.255.255.255".matches(regexp));
+    }
+
+    @Test
+    void ipV4AddressFalseTest() {
+        String regexp = ipV4Address();
+        assertFalse("0.0.0".matches(regexp));
+        assertFalse("0.0.0+0".matches(regexp));
+        assertFalse("0".matches(regexp));
+        assertFalse("0.-".matches(regexp));
+        assertFalse("0.0.0 0".matches(regexp));
+    }
+
+    @Test
+    void stringWithJavaNamesTest() {
+        String names1 = "123 1a _ abs int enum null lmn";
+        assertEquals("abs lmn", stringWithJavaNames(names1));
+        String names2 = "123 1a _ int enum null lmn";
+        assertEquals("lmn", stringWithJavaNames(names2));
+        String names3 = "123 1a _ int enum null";
+        assertEquals("", stringWithJavaNames(names3));
     }
 }
